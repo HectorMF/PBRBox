9b5651159caafca754d23b1adb7defe16b39fcd3
Hector Medina-Fetterman <medina-fetterman.1@osu.edu>
2016-11-21 16:37:12 -0500

    Added functions to compute Normals, Tangents, and Bitangents of a mesh

--- PBRBox/Geometry.cpp
+++ PBRBox/Geometry.cpp
@@ -135,12 +135,11 @@ void Geometry::uploadToGPU()
 	std::vector<PackedVertex> gpuVertices; 
 
 	if (m_normals.size() < getNumVertices())
-		m_normals.resize(getNumVertices());
-
-	if(m_texCoords.size() < getNumVertices())
-		m_texCoords.resize(getNumVertices());
+		computeNormals();
 	if (m_tangents.size() < getNumVertices())
 		computeTangents();
+	if(m_texCoords.size() < getNumVertices())
+		m_texCoords.resize(getNumVertices());
 
 	for (int i = 0; i < getNumVertices(); i++)
 	{
@@ -188,4 +187,114 @@ void Geometry::uploadToGPU()
 
 
 	glBindVertexArray(0);
+}
+
+void Geometry::computeTangents()
+{
+	m_tangents.clear();
+	m_bitangents.clear();
+
+	m_tangents.resize(getNumVertices());
+	m_bitangents.resize(getNumVertices());
+
+	int triangleOffset = 0;
+	for (int i = 0; i < m_indices.size() / 3; i++)
+	{
+		unsigned int i1 = m_indices[triangleOffset + 0];
+		unsigned int i2 = m_indices[triangleOffset + 1];
+		unsigned int i3 = m_indices[triangleOffset + 2];
+
+		// Shortcuts for vertices
+		glm::vec3 & v1 = m_vertices[i1];
+		glm::vec3 & v2 = m_vertices[i2];
+		glm::vec3 & v3 = m_vertices[i3];
+
+		// Shortcuts for UVs
+		glm::vec2 & w1 = m_texCoords[i1];
+		glm::vec2 & w2 = m_texCoords[i2];
+		glm::vec2 & w3 = m_texCoords[i3];
+
+		// position differences p1->p2 and p1->p3
+		glm::vec3 v = v2 - v1;
+		glm::vec3 w = v3 - v1;
+
+		// texture offset p1->p2 and p1->p3
+		float sx = w2.x - w1.x, sy = w2.y - w1.y;
+		float tx = w3.x - w1.x, ty = w3.y - w1.y;
+		float dirCorrection = (tx * sy - ty * sx) < 0.0f ? -1.0f : 1.0f;
+		// when t1, t2, t3 in same position in UV space, just use default UV direction.
+		if (0 == sx && 0 == sy && 0 == tx && 0 == ty) {
+			sx = 0.0; sy = 1.0;
+			tx = 1.0; ty = 0.0;
+		}
+
+		// tangent points in the direction where to positive X axis of the texture coord's would point in model space
+		// bitangent's points along the positive Y axis of the texture coord's, respectively
+		glm::vec3 tangent, bitangent;
+		tangent.x = (w.x * sy - v.x * ty) * dirCorrection;
+		tangent.y = (w.y * sy - v.y * ty) * dirCorrection;
+		tangent.z = (w.z * sy - v.z * ty) * dirCorrection;
+
+		bitangent.x = (w.x * sx - v.x * tx) * dirCorrection;
+		bitangent.y = (w.y * sx - v.y * tx) * dirCorrection;
+		bitangent.z = (w.z * sx - v.z * tx) * dirCorrection;
+
+		m_tangents[i1] += tangent;
+		m_tangents[i2] += tangent;
+		m_tangents[i3] += tangent;
+
+		m_bitangents[i1] += bitangent;
+		m_bitangents[i2] += bitangent;
+		m_bitangents[i3] += bitangent;
+
+		triangleOffset += 3;
+	}
+
+	for (int v = 0; v < m_vertices.size(); v++)
+	{
+		glm::vec3 n = m_normals[v];
+		glm::vec3 t = m_tangents[v];
+		glm::vec3 b = m_bitangents[v];
+
+		// Gram-Schmidt orthogonalize
+		t = glm::normalize(t - n * glm::dot(n, t));
+		b = glm::normalize(b - n * glm::dot(n, b));
+
+		if (glm::dot(glm::cross(n, t), b) < 0.0f)
+			t = t * -1.0f;
+
+		m_tangents[v] = t;
+		m_bitangents[v] = b;
+	}
+}
+
+void Geometry::computeNormals()
+{
+	m_normals.clear();
+	m_normals.resize(getNumVertices());
+
+	//for (int i = 0; i < m_vertices.size(); i++)
+	//	m_normals.push_back(glm::vec3(0));
+	unsigned int triangleOffset = 0;
+	for (int i = 0; i < m_indices.size() / 3; i++)
+	{
+		unsigned int i1 = m_indices[triangleOffset + 0];
+		unsigned int i2 = m_indices[triangleOffset + 1];
+		unsigned int i3 = m_indices[triangleOffset + 2];
+
+		glm::vec3 v1 = m_vertices[i1];
+		glm::vec3 v2 = m_vertices[i2];
+		glm::vec3 v3 = m_vertices[i3];
+
+		glm::vec3 u = v2 - v1;
+		glm::vec3 v = v3 - v1;
+
+		glm::vec3 n = glm::cross(u, v);
+
+		m_normals[i1] = glm::normalize(m_normals[i1] + n);
+		m_normals[i2] = glm::normalize(m_normals[i2] + n);
+		m_normals[i3] = glm::normalize(m_normals[i3] + n);
+
+		triangleOffset += 3;
+	}
 }
